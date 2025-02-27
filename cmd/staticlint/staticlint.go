4f11ce9aa8b928a2d5e7d6a479de5e9ba5d879fe
Maynagashev <lifesup@gmail.com>
2025-02-28 03:10:16 +0700

    Подключение анализаторов ST (stylecheck)

--- cmd/staticlint/staticlint.go
+++ cmd/staticlint/staticlint.go
@@ -12,16 +12,18 @@ import (
 	"golang.org/x/tools/go/analysis/passes/shadow"
 	"golang.org/x/tools/go/analysis/passes/structtag"
 	"honnef.co/go/tools/staticcheck"
+	"honnef.co/go/tools/stylecheck"
 
 	"github.com/maynagashev/go-metrics/cmd/staticlint/passes/errcheck"
 )
 
 // Config — имя файла конфигурации.
-const Config = `config.json`
+const Config = `cmd/staticlint/config.json`
 
 // ConfigData описывает структуру файла конфигурации.
 type ConfigData struct {
 	Staticcheck []string `json:"staticcheck"`
+	Stylecheck  []string `json:"stylecheck"`
 }
 
 func main() {
@@ -37,31 +39,57 @@ func main() {
 		errcheck.Analyzer,
 	}
 
-	// анализаторы из staticcheck.io
-	for _, analyzer := range staticcheck.Analyzers {
-		mychecks = append(mychecks, analyzer.Analyzer)
-	}
-
 	// Пытаемся прочитать конфигурационный файл
 	data, err := os.ReadFile(Config)
 	if err == nil {
 		var cfg ConfigData
 		if err = json.Unmarshal(data, &cfg); err == nil {
-			// Добавляем анализаторы из staticcheck, которые указаны в файле конфигурации
-			checks := make(map[string]bool)
-			for _, v := range cfg.Staticcheck {
-				checks[v] = true
-			}
-			for _, analyzer := range staticcheck.Analyzers {
-				if checks[analyzer.Analyzer.Name] {
+			// Добавляем анализаторы из staticcheck, которые указаны в файле конфигурации или все
+			if len(cfg.Staticcheck) > 0 {
+				checks := make(map[string]bool)
+				for _, v := range cfg.Staticcheck {
+					checks[v] = true
+				}
+				for _, analyzer := range staticcheck.Analyzers {
+					if checks[analyzer.Analyzer.Name] {
+						mychecks = append(mychecks, analyzer.Analyzer)
+					}
+				}
+			} else {
+				log.Println("Используются все анализаторы SA*** (т.к. в конфигурационном файле не указаны анализаторы staticcheck)")
+				// все анализаторы из staticcheck.io
+				for _, analyzer := range staticcheck.Analyzers {
 					mychecks = append(mychecks, analyzer.Analyzer)
 				}
 			}
+
+			// Добавляем анализаторы из stylecheck, которые указаны в файле конфигурации
+			if len(cfg.Stylecheck) > 0 {
+				stChecks := make(map[string]bool)
+				for _, v := range cfg.Stylecheck {
+					stChecks[v] = true
+				}
+				for _, analyzer := range stylecheck.Analyzers {
+					if stChecks[analyzer.Analyzer.Name] {
+						mychecks = append(mychecks, analyzer.Analyzer)
+					}
+				}
+				log.Println("Используются анализаторы ST**** из конфигурационного файла:", cfg.Stylecheck)
+			} else {
+				log.Println("Используются все анализаторы ST**** (т.к. в конфигурационном файле не указаны анализаторы stylecheck)")
+				for _, analyzer := range stylecheck.Analyzers {
+					mychecks = append(mychecks, analyzer.Analyzer)
+				}
+			}
+		} else {
+			log.Println("Ошибка при чтении конфигурационного файла:", err)
 		}
+	} else {
+		log.Println("Конфигурационный файл не найден, используются все анализаторы")
 	}
 
 	// выводим список анализаторов, короткий список c нумерацией и однострочным описанием
-	log.Println("Включает в себя следующие анализаторы:")
+	log.Println("Итоговый список анализаторов:")
 	for i, analyzer := range mychecks {
 		description := analyzer.Doc
 		// берем только первую строку описания
@@ -71,7 +99,7 @@ func main() {
 		log.Printf("%d. %s: %s\n", i+1, analyzer.Name, description)
 	}
 
-	log.Println("Запуск анализатора...")
+	log.Println("Запуск мультичекера...")
 	multichecker.Main(
 		mychecks...,
 	)
